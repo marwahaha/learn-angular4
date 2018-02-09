73cd8073e179a892dd0894620d2292a9b7e34b1b
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-02-09 17:23:01 -0200

    update app component

--- index/src/app/app.component.ts
+++ index/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  private statusGame: boolean = true;
+  private message: String = "You lost!";
+
+  public endGame(status: boolean): void {
+    console.log("Status", typeof status, status);
+    this.statusGame = false;
+
+    if (status) {
+      this.message = "You win!";
+    }
+
+    console.log("End Game!");
+  }
+
+  public newGame(): void {
+    this.statusGame = true;
+  }
 }
--- index/src/app/app.module.ts
+++ index/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { PanelComponent } from './panel/panel.component';
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressbarComponent } from './progressbar/progressbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, PanelComponent, AttemptsComponent, ProgressbarComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
