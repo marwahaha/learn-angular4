cc2762f82eada88022c8a2f0017ede2e6cd29dc7
Ademílson F. Tonato <ademilsonft@outlook.com>
2018-02-18 21:44:53 -0300

    transfor simple request to observables with switchMap and debounceTime

--- bird-store/src/app/header/header.component.ts
+++ bird-store/src/app/header/header.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OffersService } from "../offers.service";
+import { Offer } from "../shared/offer.model";
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+import "../utils/util.extension";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
+  providers: [OffersService]
 })
 export class HeaderComponent implements OnInit {
+  private offersService: OffersService;
+  private searchSubject: Subject<string> = new Subject<string>();
+  public offers: Observable<Offer[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(offersService: OffersService) {
+    this.offersService = offersService;
   }
 
+  ngOnInit() {
+    this.offers = this.searchSubject
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap((term: string) => {
+        if (term.trim() === "") {
+          return Observable.of<Offer[]>([]);
+        }
+        return this.offersService.findOfferByTerm(term);
+      })
+      .catch((error: any) => {
+        console.error("Error Observable: ", error);
+
+        return Observable.of<Offer[]>([]);
+      });
+
+    this.offers.subscribe(
+      (offers: Offer[]) => console.log("Offers: ", offers),
+      (error: any) =>
+        console.error("Error status: ", error.status, "Error details: ", error),
+      () => console.log("Observable findOfferByTerm completed")
+    );
+  }
+
+  public search(term: string): void {
+    this.searchSubject.next(term);
+
+    // this.offers = this.offersService.findOfferByTerm(term);
+    // this.offers.subscribe(
+    //   (offers: Offer[]) => console.log("Offers: ", offers),
+    //   (error: any) =>
+    //     console.error("Error status: ", error.status, "Error details: ", error),
+    //   () => console.log("Observable findOfferByTerm completed")
+    // );
+  }
 }
--- bird-store/src/app/offers.service.ts
+++ bird-store/src/app/offers.service.ts
@@ -1,8 +1,13 @@
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
 import { Injectable } from "@angular/core";
-import "rxjs/add/operator/toPromise";
+
 import { Offer } from "./shared/offer.model";
 import { URL_API } from "./app.api";
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/toPromise";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/retry";
 
 @Injectable()
 export class OffersService {
@@ -12,14 +17,14 @@ export class OffersService {
     return this.http
       .get(`${URL_API}offers${category}`)
       .toPromise()
-      .then((response: any) => response.json());
+      .then((response: Response) => response.json());
   }
 
   public getOfferById(id: number): Promise<Offer> {
     return this.http
       .get(`${URL_API}offers/${id}`)
       .toPromise()
-      .then((response: any) => response.json());
+      .then((response: Response) => response.json());
   }
 
   public getOfferDescriptionsById(
@@ -29,6 +34,13 @@ export class OffersService {
     return this.http
       .get(`${URL_API}${description}/${id}`)
       .toPromise()
-      .then((response: any) => response.json().description);
+      .then((response: Response) => response.json().description);
+  }
+
+  public findOfferByTerm(term: string): Observable<Offer[]> {
+    return this.http
+      .get(`${URL_API}offers?description_like=${term}`)
+      .retry(10)
+      .map((response: Response) => response.json());
   }
 }
